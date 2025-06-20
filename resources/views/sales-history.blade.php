e03b196be91f237f37404e59ca11619a458617e3
zawayakurma <jadilahpemimpin@gmail.com>
2025-06-20 21:44:35 +0700

    feat: have been created ui for dashboard, sales-history, products data.

--- resources/views/sales-history.blade.php
+++ resources/views/sales-history.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Riwayat Penjualan Produk') }}
+            Riwayat Penjualan
         </h2>
     </x-slot>
 
